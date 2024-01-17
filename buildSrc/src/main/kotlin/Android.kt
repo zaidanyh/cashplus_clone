6dbb1b096d76b2754fbbd811b160b8144f5d6d02
yoga <prayogaananda04@gmail.com>
2024-01-17 21:41:53 +0700

    Delete files that should not be pushed in git

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -1,9 +1,9 @@
 object Android {
     const val appId = "com.pasukanlangit.cashplus"
-    const val compileSdk = 33
+    const val compileSdk = 34
     const val buildTools = "30.0.3"
     const val minSdk = 23
-    const val targetSdk = 33
+    const val targetSdk = 34
     const val versionCode = 310
     const val versionName = "2.6.0"
 }
